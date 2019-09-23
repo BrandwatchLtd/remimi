c979645551c862c9843485cc3264a11e4e0a895a
George Thomas <gthomas@brandwatch.com>
2019-09-23 10:25:25 +0100

    Revert "feat: handle timeEvent with action type formatter"
    This reverts commit 68938cd489578833b15b7fd931ca092a6ef41f12.

--- test/mixpanelMiddleware.tests.js
+++ test/mixpanelMiddleware.tests.js
@@ -300,11 +300,6 @@ describe('mixpanelMiddleware', () => {
                 runMiddleware(mixpanelActionWithProps, {actionTypeFormatter: value => `---${value}---`});
                 assert.equal(mixpanel.track.firstCall.args[0], '---fooEvent---');
             });
-
-            it('formats time event', function() {
-                runMiddleware(mixpanelActionWithTimeEvent, {actionTypeFormatter: value => `---${value}---`});
-                assert.equal(mixpanel.time_event.firstCall.args[0], '---barEvent---');
-            });
         });
 
         describe('properties', function() {
